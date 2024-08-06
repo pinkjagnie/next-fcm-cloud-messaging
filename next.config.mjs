96ead1129666643a818309358c8349cef768a1bb
pinkjagnie <olga.krzysztofik@gmail.com>
2024-08-06 17:17:32 +0200

    Added env variables

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_APP_API_KEY: process.env.NEXT_APP_API_KEY,
+    NEXT_APP_AUTH_DOMAIN: process.env.NEXT_APP_AUTH_DOMAIN,
+    NEXT_APP_PROJECT_ID: process.env.NEXT_APP_PROJECT_ID,
+    NEXT_APP_STORAGE_BUCKET: process.env.NEXT_APP_STORAGE_BUCKET,
+    NEXT_APP_MESSAGING_SENDER_ID: process.env.NEXT_APP_MESSAGING_SENDER_ID,
+    NEXT_APP_APP_ID: process.env.NEXT_APP_APP_ID,
+    NEXT_APP_MEASUREMENT_ID: process.env.NEXT_APP_MEASUREMENT_ID,
+    NEXT_APP_VAPID_KEY: process.env.NEXT_APP_VAPID_KEY,
+  },
+};
 
 export default nextConfig;
