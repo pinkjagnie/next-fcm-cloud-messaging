35eaf0b8aaa7f1bb2bea148530de4aa155c915c2
pinkjagnie <olga.krzysztofik@gmail.com>
2024-08-07 13:46:12 +0200

    Fixed issue with displaying toast with message

--- src/app/page.js
+++ src/app/page.js
@@ -11,34 +11,60 @@ import "react-toastify/dist/ReactToastify.css";
 import Message from "./components/Message";
 
 export default function Home() {
-  async function requestPermission() {
-    //requesting permission using Notification API
-    const permission = await Notification.requestPermission();
-
-    if (permission === "granted") {
-      requestForToken();
-    } else if (permission === "denied") {
-      //notifications are blocked
-      alert("You denied for the notification");
-    }
-  }
-
   useEffect(() => {
+    async function requestPermission() {
+      //requesting permission using Notification API
+      const permission = await Notification.requestPermission();
+
+      if (permission === "granted") {
+        requestForToken();
+
+        onMessage(messaging, (payload) => {
+          console.log("incoming message");
+          console.log(payload);
+          toast(<Message notification={payload.notification} />, {
+            autoClose: false,
+            closeOnClick: false,
+            closeButton: true,
+          });
+        });
+      } else if (permission === "denied") {
+        //notifications are blocked
+        alert("You denied for the notification");
+      }
+    }
+
     requestPermission();
-
-    const unsubscribe = onMessage(messaging, (payload) => {
-      console.log("incoming message");
-      console.log(payload);
-      toast(<Message notification={payload.notification} />, {
-        autoClose: false,
-        closeOnClick: false,
-        closeButton: true,
-      });
-    });
-
-    // Clean up the subscription on unmount
-    return () => unsubscribe();
   }, []);
+
+  // async function requestPermission() {
+  //   //requesting permission using Notification API
+  //   const permission = await Notification.requestPermission();
+
+  //   if (permission === "granted") {
+  //     requestForToken();
+  //   } else if (permission === "denied") {
+  //     //notifications are blocked
+  //     alert("You denied for the notification");
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   requestPermission();
+
+  //   const unsubscribe = onMessage(messaging, (payload) => {
+  //     console.log("incoming message");
+  //     console.log(payload);
+  //     toast(<Message notification={payload.notification} />, {
+  //       autoClose: false,
+  //       closeOnClick: false,
+  //       closeButton: true,
+  //     });
+  //   });
+
+  //   // Clean up the subscription on unmount
+  //   return () => unsubscribe();
+  // }, []);
 
   return (
     <main className="min-h-screen py-24 px-6">
