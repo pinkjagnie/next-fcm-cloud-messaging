ac6186edb3cfeea64f56bac1aa76f7bdf2646f44
pinkjagnie <olga.krzysztofik@gmail.com>
2024-08-08 13:27:31 +0200

    Second way to solve about service worker

--- public/firebaseConfig.js
+++ public/firebaseConfig.js
@@ -113,12 +113,37 @@ const firestore = getFirestore(app);
 //     });
 // };
 
-navigator.serviceWorker.register("/firebase-messaging-sw.js", {
-  scope: "/firebase-cloud-messaging-push-scope",
-});
+// First way to solve about service worker - this works!
+// navigator.serviceWorker.register("/firebase-messaging-sw.js", {
+//   scope: "/firebase-cloud-messaging-push-scope",
+// });
+
+// Second way to solve about service worker - this works too!
+const getOrRegisterServiceWorker = () => {
+  if ("serviceWorker" in navigator) {
+    return window.navigator.serviceWorker
+      .getRegistration("/firebase-cloud-messaging-push-scope")
+      .then((serviceWorker) => {
+        if (serviceWorker) {
+          return serviceWorker;
+        }
+        return window.navigator.serviceWorker
+          .register("/firebase-messaging-sw.js")
+          .then((serviceWorker) => {
+            console.log("success registering SW");
+          })
+          .catch((err) => {
+            console.log("registering failed", err);
+          });
+      });
+  }
+  throw new Error("The browser doesn`t support service worker.");
+};
 
 export const requestForToken = async () => {
   try {
+    const serviceWorkerRegistration = await getOrRegisterServiceWorker();
+
     const currentToken = await getToken(messaging, {
       vapidKey: process.env.NEXT_APP_VAPID_KEY,
     });
