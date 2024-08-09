47b9db7b5bc949e43d718e1f70aeedf260c3d5af
pinkjagnie <olga.krzysztofik@gmail.com>
2024-08-10 01:35:46 +0200

    Added button for sending push messages to a specific token

--- src/app/components/SingleToken.js
+++ src/app/components/SingleToken.js
@@ -1,7 +1,12 @@
 const SingleToken = ({ token }) => {
   return (
-    <li className="py-1 px-2 bg-gray-100 rounded mb-2 border border-gray-300 break-words">
-      {token}
+    <li className="flex items-center justify-between w-[90%] mx-auto pb-4">
+      <span className="w-[85%] py-1 px-2 bg-gray-100 rounded mb-2 border border-gray-300 break-words">
+        {token}
+      </span>
+      <button className="ml-4 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">
+        Push message
+      </button>
     </li>
   );
 };
